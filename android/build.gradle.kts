29deaec091610e106dd9910e91d31410e6732e73
João Paulo Debian 17/11/2024 <joaopaulofaria98@gmail.com>
2026-03-12 10:02:01 -0300

    chore(app): bootstrap flutter project structure

---
+++ android/build.gradle.kts
@@ -0,0 +1,31 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    // ... outros plugins como id("com.android.application") version "..." apply false
+    
+    // Adicione esta linha para registrar o plugin do Google
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
